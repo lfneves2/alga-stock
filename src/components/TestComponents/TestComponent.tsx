49e2d1310c8bbf67f8b0fa780c78bcc455e0bb4d
Leandro Felix <felix.neves2@hotmail.com>
2020-09-17 20:16:50 -0300

    Criado o primeiro component e movido o componente app de diretorio.

---
+++ src/components/TestComponents/TestComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './TestComponent.css'
+
+const TestComponent = () => (
+    <div className="TestComponent">Test Component</div>
+)
+
+export default TestComponent;

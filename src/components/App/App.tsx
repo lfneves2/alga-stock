661738630c36aafd78b45e8cd590de75eb8f4871
Leandro Felix <felix.neves2@hotmail.com>
2020-09-23 20:15:45 -0300

    Limpando componente App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,35 +2,16 @@ import React, { useState } from 'react';
 
 import Header from '../Header';
 
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
 
 import './App.css';
-import Input from '../../shared/Input';
-
-function TestComponent() {
-  return <img width="12" src="https://img2.gratispng.com/20181203/otz/kisspng-magnifying-glass-computer-icons-zoom-lens-scalable-search-shop-svg-png-icon-free-download-33415-5c05ad5bce5493.9552953015438759318451.jpg" alt="Search icon"/>
-}
 
 function App() {
-
-  const[street, setStreet] = useState('');
-
   return (
     <div className="App">
       <Header title="AlgaStock"/>
-
       <Container >
-        <ul>
-
-          {
-            ['Leandro','Karoline','Amanda'].map((name,index) => {
-              return <li key={index}>
-                {name}
-              </li>
-            })
-          }
-        </ul>
+        
 
       </Container>
     </div>
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Table.css';
+
+const Table = () => {
+    return <table>
+        
+    </table>;
+}
+
+export default Table;

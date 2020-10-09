5e72c6fc06b7922fca9b9a89d8091060471915dd
Leandro Felix <felix.neves2@hotmail.com>
2020-10-09 13:26:36 -0300

    inserindo ação de novo produto com redux com dados offline

--- src/components/Products/ProductsCRUD.tsx
+++ src/components/Products/ProductsCRUD.tsx
@@ -4,7 +4,8 @@ import { createSingleProduct, deleteProducts, getAllProducts, updateSingleProduc
 import Table, { TableHeader } from '../../shared/Table/Table';
 import { Product } from '../../shared/Table/Table.mockdata';
 import ProductForm, { ProductCreator } from './ProductForm';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
+import { insertNewProduct } from '../../redux/Products/Products.Action';
 
 const headers: TableHeader[] = [
     {key: 'id', value: '#'},
@@ -18,6 +19,7 @@ declare interface ProductsCRUDProps {
 }
 
 const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
+    const dispatch = useDispatch();
 
     //const [products, setProducts] = useState<Product[]>([]);
     const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>();
@@ -33,7 +35,7 @@ const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
 
     const handlerProductSubmit = async (product: ProductCreator) => {
         try {
-          await createSingleProduct(product);
+          dispatch(insertNewProduct(product))
           fetchData();
         }catch (err) {
           Swal.fire('Oops!', err.message, 'error');

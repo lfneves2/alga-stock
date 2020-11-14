27474c6c789aa183feeb6e917421e18bff7166ab
Leandro Felix <felix.neves2@hotmail.com>
2020-11-14 14:01:09 -0300

    redux thunk + typescript

--- src/components/Products/ProductsCRUD.tsx
+++ src/components/Products/ProductsCRUD.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
-import { createSingleProduct, deleteProducts, getAllProducts, updateSingleProduct } from '../../services/Products.service';
+import { deleteProducts, getAllProducts } from '../../services/Products.service';
 import Table, { TableHeader } from '../../shared/Table/Table';
 import { Product } from '../../shared/Table/Table.mockdata';
 import ProductForm, { ProductCreator } from './ProductForm';
 import { connect, useDispatch } from 'react-redux';
-import { getProducts, insertNewProduct } from '../../redux/Products/Products.Action';
+import { getProducts, insertNewProduct, updateProducts } from '../../redux/Products/Products.action';
 
 const headers: TableHeader[] = [
     {key: 'id', value: '#'},
@@ -27,7 +27,7 @@ const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
     async function fetchData() {
       try {
         await dispatch(getProducts());
-        Swal.fire('Uhul!', 'Fetch done', 'success');
+        Swal.fire('Uhul','Fetch done', 'success')
       } catch (err){
         Swal.fire('Oops!', err.message, 'error');
       }
@@ -38,63 +38,62 @@ const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
     }, [])
 
     const handlerProductSubmit = async (product: ProductCreator) => {
-        try {
-          dispatch(insertNewProduct(product))
-          fetchData();
-        }catch (err) {
-          Swal.fire('Oops!', err.message, 'error');
+      try {
+        dispatch(insertNewProduct(product))
+        fetchData();
+      }catch (err) {
+        Swal.fire('Oops!', err.message, 'error');
+      }
+    }
+  
+    const handlerProductUpdate = async (newProduct: Product) => {
+      try{
+        await dispatch(updateProducts(newProduct));
+      } catch (err) {
+        Swal.fire('Oops!', err.message, 'error');
+      }
+  
+      setUpdatingProduct(undefined)
+    }
+  
+    const handlerProductEdit = (product: Product) => {
+      console.log(product)
+      setUpdatingProduct(product);
+    }
+  
+    const handlerProductDetail = (product: Product) => {
+      Swal.fire(
+        'Product Detail',
+        `${product.name} costs $${product.price} we have avaible ${product.stock} in stock`,
+        'info'
+      )
+    }
+  
+    const deleteProduct = async(id: string) => {
+      try {
+        await deleteProducts(id);
+        fetchData();
+        Swal.fire('Uhul!', 'Product successfully deleted', 'success');
+      } catch (err) {
+        Swal.fire('Oops!', err.message, 'error');
+      }
+    }
+  
+    const handlerProductDelete =  (product: Product) => {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#09f',
+        cancelButtonColor: '#d33',
+        confirmButtonText: `Yes, delete ${product.name}!`
+      }).then((result) => {
+        if (result.isConfirmed) {
+          deleteProduct(product._id);
         }
-      }
-    
-      const handlerProductUpdate = async (newProduct: Product) => {
-        try{
-          await updateSingleProduct(newProduct);
-          fetchData();
-        } catch (err) {
-          Swal.fire('Oops!', err.message, 'error');
-        }
-    
-        setUpdatingProduct(undefined)
-      }
-    
-      const handlerProductEdit = (product: Product) => {
-        console.log(product)
-        setUpdatingProduct(product);
-      }
-    
-      const handlerProductDetail = (product: Product) => {
-        Swal.fire(
-          'Product Detail',
-          `${product.name} costs $${product.price} we have avaible ${product.stock} in stock`,
-          'info'
-        )
-      }
-    
-      const deleteProduct = async(id: string) => {
-        try {
-          await deleteProducts(id);
-          fetchData();
-          Swal.fire('Uhul!', 'Product successfully deleted', 'success');
-        } catch (err) {
-          Swal.fire('Oops!', err.message, 'error');
-        }
-      }
-    
-      const handlerProductDelete =  (product: Product) => {
-        Swal.fire({
-          title: 'Are you sure?',
-          text: "You won't be able to revert this!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#09f',
-          cancelButtonColor: '#d33',
-          confirmButtonText: `Yes, delete ${product.name}!`
-        }).then((result) => {
-          if (result.isConfirmed) {
-            deleteProduct(product._id);
-          }
-        })
-      }
+      })
+    }
     
     return <>
         <Table 
--- src/redux/Products/Products.Action.ts
+++ src/redux/Products/Products.Action.ts
@@ -1,9 +1,14 @@
 import { Action, Thunk } from "..";
 import { ProductCreator } from "../../components/Products/ProductForm"
-import { getAllProducts } from "../../services/Products.service"
+import { getAllProducts, updateSingleProduct } from "../../services/Products.service"
 import { Product } from "../../shared/Table/Table.mockdata";
 
-export const getProducts = (): Thunk<Product[]> => async (dispatch: any)=> {
+export const updateProducts = (newProduc: Product): Thunk => async (dispatch) => {
+    await updateSingleProduct(newProduc);
+    dispatch(getAllProducts);
+}
+
+export const getProducts = (): Thunk<Product[]> => async (dispatch)=> {
     const products = await getAllProducts();
     dispatch({
         type: 'FETCH_PRODUCTS',
